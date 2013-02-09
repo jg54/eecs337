e2c208fdf7c1a94032b2cab8cf2fd15b03134b01
Asmaa Aljuhani <asmaaaljuhani2012@u.northwestern.edu>
2013-02-09 14:00:22 -0600

    get reward and movie

--- academyawards/playwithregularexp.py
+++ academyawards/playwithregularexp.py
@@ -1,12 +1,31 @@
 import re
 
-entity = 'Damien Lewis'
+def find_between( s, first, last ):
+    try:
+        start = s.index( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
+
+
+entity = 'Hugh Jackman'
 
 f = open('clearTweets.txt')
 for tweet in f:
     # this is the best reg exp for getting the winners
     if re.findall( entity+'.(wins|won).best.(actor|actress).in.*for.*#' ,tweet , re.M|re.I):
         print(tweet)
+        # Entity name
+        print entity
+        #Reward
+        print "Rewards"
+        print find_between(re.findall(r'[\W]in.*for',tweet , re.M|re.I)[0], "in" , "for")
+        #Movie
+        print "Movie"
+        print find_between(re.findall(r'[\W]for.*#',tweet , re.M|re.I)[0], "for" , "#")
+        
 
 
     
@@ -17,3 +36,4 @@ for tweet in f:
     #for nominees
     #if re.findall( r'I HOPE.*WINS' ,tweet , re.M|re.I):
         
+
